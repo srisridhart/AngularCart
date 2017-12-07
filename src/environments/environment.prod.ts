7dd49116a1c0f6fd3d60d258f6e2fa82d492a1e2
Sridhar Thirumal <sridhart@ilink-systems.com>
2017-12-07 11:24:10 +0530

    Initial commit with draft authentication

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD7g4EILzDf9zNiJnr7jDVC-ll9sK06Cco",
+    authDomain: "oshop-e055d.firebaseapp.com",
+    databaseURL: "https://oshop-e055d.firebaseio.com",
+    projectId: "oshop-e055d",
+    storageBucket: "",
+    messagingSenderId: "1048260730229"
+  }
+};
